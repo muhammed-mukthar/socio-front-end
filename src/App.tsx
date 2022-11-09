d78e0b1ddfb49ea076259d8340f821d006b9a529
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-11-09 12:18:25 +0530

    changing to js

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
   RouterProvider,
   Route,
   Outlet,
+  Navigate,
 } from "react-router-dom";
 import Register from './pages/register/Register';
 import NavBar from './components/navBar/NavBar';
@@ -15,7 +16,7 @@ import RightBar from './components/rightBar/RightBar';
 import Home from './pages/home/Home';
 import Profile from './pages/profile/Profile';
 function App() {
-
+  const currentUser=false
   const Layout=():any=>{
     return(
     <div>
@@ -28,11 +29,17 @@ function App() {
       </div >
     </div>)
   }
+  const ProtectedRoute=({children})=>{
+    if(!currentUser){
+      return <Navigate to='/login'></Navigate>
+    }
+    return children
+  }
 
   const router = createBrowserRouter([
     {
       path:"/",
-      element:<Layout/>,
+      element: <ProtectedRoute><Layout/></ProtectedRoute> ,
       children:[{
         path:"/",
         element:<Home/>
